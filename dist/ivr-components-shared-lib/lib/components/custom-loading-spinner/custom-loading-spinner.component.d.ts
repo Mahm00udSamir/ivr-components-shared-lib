68d64baa11907a861fb667658370b9d7e9185ce0
msamir6 <m.samir@injaz-consulting.com>
2026-05-05 21:45:57 +0300

    remove v2.7

--- dist/ivr-components-shared-lib/lib/components/custom-loading-spinner/custom-loading-spinner.component.d.ts
+++
@@ -1,6 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class CustomLoadingSpinnerComponent {
-    size: import("@angular/core").InputSignal<"small" | "medium" | "large">;
-    static ɵfac: i0.ɵɵFactoryDeclaration<CustomLoadingSpinnerComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CustomLoadingSpinnerComponent, "custom-loading-spinner", never, { "size": { "alias": "size"; "required": false; "isSignal": true; }; }, {}, never, never, true, never>;
-}
