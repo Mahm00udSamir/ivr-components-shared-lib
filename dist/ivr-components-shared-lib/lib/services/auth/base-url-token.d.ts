135867bef9a02bc3ac6de84ad3eb4b22c53f8a71
Norhan Shehab <norhansshehab@gmail.com>
2026-09-15 21:54:22 +0300

    fix: update API_BASE_URL to new endpoint and ensure consistent formatting in base-url-token.d.ts

--- dist/ivr-components-shared-lib/lib/services/auth/base-url-token.d.ts
+++ dist/ivr-components-shared-lib/lib/services/auth/base-url-token.d.ts
@@ -1,3 +1,3 @@
-import { InjectionToken } from "@angular/core";
+import { InjectionToken } from '@angular/core';
 export declare const API_BASE_URL: InjectionToken<string>;
 export declare const CHAT_BASE_URL: InjectionToken<string>;
